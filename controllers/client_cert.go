f8506b58b97d34ea07436c120301de07002db807
Alexander Bartolomey <occloxium@gmail.com>
2022-09-08 15:34:47 +0200

    refactor: add check for existence of client cert on secret object
    Signed-off-by: Alexander Bartolomey <occloxium@gmail.com>

--- controllers/client_cert.go
+++ controllers/client_cert.go
@@ -2,9 +2,11 @@ package controllers
 
 import (
 	"context"
+	"errors"
 
 	corev1 "k8s.io/api/core/v1"
 	"k8s.io/apimachinery/pkg/types"
+	"sigs.k8s.io/controller-runtime/pkg/log"
 )
 
 func (r *KubeconfigReconciler) ClusterCA(ctx context.Context, namespace string) (string, error) {
@@ -19,12 +21,17 @@ func (r *KubeconfigReconciler) ClusterCA(ctx context.Context, namespace string) 
 }
 
 func (r *KubeconfigReconciler) ClientData(ctx context.Context, object types.NamespacedName) (string, string, error) {
+	l := log.FromContext(ctx)
+
 	clientKeySecret := &corev1.Secret{}
 	err := r.Get(ctx, object, clientKeySecret)
 	if err != nil {
 		return "", "", err
 	}
 	clientKey := clientKeySecret.Data["tls.key"]
-	clientCert := clientKeySecret.Data["tls.crt"]
+	clientCert, ok := clientKeySecret.Data["tls.crt"]
+	if !ok {
+		l.V(2).Error(errors.New("no certificate found in secret"), "Secret does not contain certificate key")
+	}
 	return string(clientKey), string(clientCert), nil
 }
