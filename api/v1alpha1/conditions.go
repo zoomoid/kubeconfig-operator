6e424ca1cf81f3a1f3e57d7ea4ba05b5db67887c
Alexander Bartolomey <occloxium@gmail.com>
2022-09-28 11:36:26 +0200

    chore: cleanup old conditions
    Signed-off-by: Alexander Bartolomey <occloxium@gmail.com>

--- api/v1alpha1/conditions.go
+++ api/v1alpha1/conditions.go
@@ -1,14 +1,5 @@
 package v1alpha1
 
-import (
-	m "k8s.io/apimachinery/pkg/api/meta"
-	metav1 "k8s.io/apimachinery/pkg/apis/meta/v1"
-)
-
-// type Condition metav1.Condition
-
-// type ConditionList []Condition
-
 const (
 	ConditionTypeCSRCreated              string = "CSRCreated"
 	ConditionTypeCSRApproved             string = "CSRApproved"
@@ -18,61 +9,61 @@ const (
 	ConditionTypeKubeconfigFinished      string = "KubeconfigFinished"
 )
 
-// DefaultConditions adds fresh conditions if their type does not yet exist
-func DefaultConditions(cl []metav1.Condition) []metav1.Condition {
-	if m.FindStatusCondition(cl, ConditionTypeCSRCreated) == nil {
-		cl = append(cl, metav1.Condition{
-			Type:               ConditionTypeCSRCreated,
-			Status:             metav1.ConditionUnknown,
-			LastTransitionTime: metav1.Now(),
-			Reason:             "n/a",
-			Message:            "n/a",
-		})
-	}
-	if m.FindStatusCondition(cl, ConditionTypeCSRApproved) == nil {
-		cl = append(cl, metav1.Condition{
-			Type:               ConditionTypeCSRApproved,
-			Status:             metav1.ConditionUnknown,
-			LastTransitionTime: metav1.Now(),
-			Reason:             "n/a",
-			Message:            "n/a",
-		})
-	}
-	if m.FindStatusCondition(cl, ConditionTypeUserSecretCreated) == nil {
-		cl = append(cl, metav1.Condition{
-			Type:               ConditionTypeUserSecretCreated,
-			Status:             metav1.ConditionUnknown,
-			LastTransitionTime: metav1.Now(),
-			Reason:             "n/a",
-			Message:            "n/a",
-		})
-	}
-	if m.FindStatusCondition(cl, ConditionTypeUserSecretFinished) == nil {
-		cl = append(cl, metav1.Condition{
-			Type:               ConditionTypeUserSecretFinished,
-			Status:             metav1.ConditionUnknown,
-			LastTransitionTime: metav1.Now(),
-			Reason:             "n/a",
-			Message:            "n/a",
-		})
-	}
-	if m.FindStatusCondition(cl, ConditionTypeKubeconfigSecretCreated) == nil {
-		cl = append(cl, metav1.Condition{
-			Type:               ConditionTypeKubeconfigSecretCreated,
-			Status:             metav1.ConditionUnknown,
-			LastTransitionTime: metav1.Now(),
-			Reason:             "n/a",
-			Message:            "n/a",
-		})
-	}
-	if m.FindStatusCondition(cl, ConditionTypeKubeconfigFinished) == nil {
-		cl = append(cl, metav1.Condition{
-			Type:               ConditionTypeKubeconfigFinished,
-			Status:             metav1.ConditionUnknown,
-			LastTransitionTime: metav1.Now(),
-			Reason:             "n/a",
-			Message:            "n/a",
-		})
-	}
-	return cl
-}
+// // defaultConditions adds fresh conditions if their type does not yet exist
+// func defaultConditions(cl []metav1.Condition) []metav1.Condition {
+// 	if meta.FindStatusCondition(cl, ConditionTypeCSRCreated) == nil {
+// 		cl = append(cl, metav1.Condition{
+// 			Type:               ConditionTypeCSRCreated,
+// 			Status:             metav1.ConditionUnknown,
+// 			LastTransitionTime: metav1.Now(),
+// 			Reason:             "n/a",
+// 			Message:            "n/a",
+// 		})
+// 	}
+// 	if meta.FindStatusCondition(cl, ConditionTypeCSRApproved) == nil {
+// 		cl = append(cl, metav1.Condition{
+// 			Type:               ConditionTypeCSRApproved,
+// 			Status:             metav1.ConditionUnknown,
+// 			LastTransitionTime: metav1.Now(),
+// 			Reason:             "n/a",
+// 			Message:            "n/a",
+// 		})
+// 	}
+// 	if meta.FindStatusCondition(cl, ConditionTypeUserSecretCreated) == nil {
+// 		cl = append(cl, metav1.Condition{
+// 			Type:               ConditionTypeUserSecretCreated,
+// 			Status:             metav1.ConditionUnknown,
+// 			LastTransitionTime: metav1.Now(),
+// 			Reason:             "n/a",
+// 			Message:            "n/a",
+// 		})
+// 	}
+// 	if meta.FindStatusCondition(cl, ConditionTypeUserSecretFinished) == nil {
+// 		cl = append(cl, metav1.Condition{
+// 			Type:               ConditionTypeUserSecretFinished,
+// 			Status:             metav1.ConditionUnknown,
+// 			LastTransitionTime: metav1.Now(),
+// 			Reason:             "n/a",
+// 			Message:            "n/a",
+// 		})
+// 	}
+// 	if meta.FindStatusCondition(cl, ConditionTypeKubeconfigSecretCreated) == nil {
+// 		cl = append(cl, metav1.Condition{
+// 			Type:               ConditionTypeKubeconfigSecretCreated,
+// 			Status:             metav1.ConditionUnknown,
+// 			LastTransitionTime: metav1.Now(),
+// 			Reason:             "n/a",
+// 			Message:            "n/a",
+// 		})
+// 	}
+// 	if meta.FindStatusCondition(cl, ConditionTypeKubeconfigFinished) == nil {
+// 		cl = append(cl, metav1.Condition{
+// 			Type:               ConditionTypeKubeconfigFinished,
+// 			Status:             metav1.ConditionUnknown,
+// 			LastTransitionTime: metav1.Now(),
+// 			Reason:             "n/a",
+// 			Message:            "n/a",
+// 		})
+// 	}
+// 	return cl
+// }
